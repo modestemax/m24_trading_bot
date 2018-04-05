7366f28c4f3eada903c5af275dbfe7d13807621b
max <modestemax@gmail.com>
2018-04-05 11:28:21 +0100

    admin

---
+++ admin/src/components/Hello.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3 class="float-right pr-3 card-title">{{msg}}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        msg: 'Bot Admin',
+      };
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
