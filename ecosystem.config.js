f8a493ddc826946b56a54c5dea167889616d07c8
max <modestemax@gmail.com>
2018-04-23 17:48:55 +0100

    ....

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,84 +6,85 @@ module.exports = {
     apps: [
 
         // First application
-        {
-            name: 'bot46',
-            script: 'app.js',
-            "exec_mode": "cluster",
-            cwd: 'trading',
-            "node_args": "--inspect=0.0.0.0:5858",
-            env: {
-                DEBUG: '*',
-                QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
-                PORT: 12346,
-                MIN_COUNT: 3,
-                SIMUL_FIRST_ENTRY: true,
-                NO_STOP_LOSS: true,
-                STOP_LOSS_PERCENT: -1.5,
-                SELL_LIMIT_PERCENT: 1.1,
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
-        },
-        {
-            name: 'bot45',
-            script: 'app.js',
-            "exec_mode": "cluster",
-            cwd: 'trading',
-            "node_args": "--inspect=0.0.0.0:5859",
-            env: {
-                DEBUG: '*',
-                QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
-                PORT: 12345,
-                MIN_COUNT: 2,
-                NO_STOP_LOSS: true,
-                STOP_LOSS_PERCENT: -1.5,
-                SELL_LIMIT_PERCENT: 1.1,
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
-        },  {
-            name: 'bot55',
-            script: 'app.js',
-            "exec_mode": "cluster",
-            cwd: 'trading',
-            "node_args": "--inspect=0.0.0.0:5859",
-            env: {
-                DEBUG: '*',
-                QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
-                PORT: 12355,
-                MIN_COUNT: 2,
-                NO_STOP_LOSS1: true,
-                STOP_LOSS_PERCENT: -1.5,
-                SELL_LIMIT_PERCENT: 1.1,
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
-        },
-//SELL_LIMIT_PERCENT:.5
-        {
-            name: 'bot48',
-            script: 'app.js',
-            "exec_mode": "cluster",
-            cwd: 'trading',
-            //"node_args": "--inspect=0.0.0.0:5858",
-            env: {
-                DEBUG: '*',
-                QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
-                PORT: 12348,
-                MIN_COUNT: 3,
-                SIMUL_FIRST_ENTRY: true,
-                NO_STOP_LOSS: true,
-                STOP_LOSS_PERCENT: -1.5,
-                SELL_LIMIT_PERCENT: .5,
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
-        },
+//         {
+//             name: 'bot46',
+//             script: 'app.js',
+//             "exec_mode": "cluster",
+//             cwd: 'trading',
+//             "node_args": "--inspect=0.0.0.0:5858",
+//             env: {
+//                 DEBUG: '*',
+//                 QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
+//                 PORT: 12346,
+//                 MIN_COUNT: 3,
+//                 SIMUL_FIRST_ENTRY: true,
+//                 NO_STOP_LOSS: true,
+//                 STOP_LOSS_PERCENT: -1.5,
+//                 SELL_LIMIT_PERCENT: 1.1,
+//             },
+//             env_production: {
+//                 NODE_ENV: 'production'
+//             }
+//         },
+//         {
+//             name: 'bot45',
+//             script: 'app.js',
+//             "exec_mode": "cluster",
+//             cwd: 'trading',
+//             "node_args": "--inspect=0.0.0.0:5859",
+//             env: {
+//                 DEBUG: '*',
+//                 QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
+//                 PORT: 12345,
+//                 MIN_COUNT: 2,
+//                 NO_STOP_LOSS: true,
+//                 STOP_LOSS_PERCENT: -1.5,
+//                 SELL_LIMIT_PERCENT: 1.1,
+//             },
+//             env_production: {
+//                 NODE_ENV: 'production'
+//             }
+//         },
+//         {
+//             name: 'bot55',
+//             script: 'app.js',
+//             "exec_mode": "cluster",
+//             cwd: 'trading',
+//             "node_args": "--inspect=0.0.0.0:5859",
+//             env: {
+//                 DEBUG: '*',
+//                 QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
+//                 PORT: 12355,
+//                 MIN_COUNT: 2,
+//                 NO_STOP_LOSS1: true,
+//                 STOP_LOSS_PERCENT: -1.5,
+//                 SELL_LIMIT_PERCENT: 1.1,
+//             },
+//             env_production: {
+//                 NODE_ENV: 'production'
+//             }
+//         },
+// //SELL_LIMIT_PERCENT:.5
+//         {
+//             name: 'bot48',
+//             script: 'app.js',
+//             "exec_mode": "cluster",
+//             cwd: 'trading',
+//             //"node_args": "--inspect=0.0.0.0:5858",
+//             env: {
+//                 DEBUG: '*',
+//                 QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
+//                 PORT: 12348,
+//                 MIN_COUNT: 3,
+//                 SIMUL_FIRST_ENTRY: true,
+//                 NO_STOP_LOSS: true,
+//                 STOP_LOSS_PERCENT: -1.5,
+//                 SELL_LIMIT_PERCENT: .5,
+//             },
+//             env_production: {
+//                 NODE_ENV: 'production'
+//             }
+//         },
         {
             name: 'bot47',
             script: 'app.js',
@@ -95,32 +96,33 @@ module.exports = {
                 QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
                 PORT: 12347,
                 MIN_COUNT: 2,
-                NO_STOP_LOSS: true,
-                STOP_LOSS_PERCENT: -1.5,
-                SELL_LIMIT_PERCENT: .5,
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
-        }, {
-            name: 'bot57',
-            script: 'app.js',
-            "exec_mode": "cluster",
-            cwd: 'trading',
-            //"node_args" "--inspect=0.0.0.0:5859",
-            env: {
-                DEBUG: '*',
-                QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
-                PORT: 12357,
-                MIN_COUNT: 2,
-                NO_STOP_LOSS: true,
-                STOP_LOSS_PERCENT: -1.5,
+                NO_STOP_LOSS: '',
+                STOP_LOSS_PERCENT: -1,
                 SELL_LIMIT_PERCENT: .5,
             },
             env_production: {
                 NODE_ENV: 'production'
             }
         },
+        // {
+        //     name: 'bot57',
+        //     script: 'app.js',
+        //     "exec_mode": "cluster",
+        //     cwd: 'trading',
+        //     //"node_args" "--inspect=0.0.0.0:5859",
+        //     env: {
+        //         DEBUG: '*',
+        //         QUOTE_CUR: 'BTC', EXCHANGE: 'binance', TIMEFRAME: 5, API_KEY: 'keys',
+        //         PORT: 12357,
+        //         MIN_COUNT: 2,
+        //         NO_STOP_LOSS: true,
+        //         STOP_LOSS_PERCENT: -1.5,
+        //         SELL_LIMIT_PERCENT: .5,
+        //     },
+        //     env_production: {
+        //         NODE_ENV: 'production'
+        //     }
+        // },
         // Second application
         // {
         //   name      : 'WEB',
